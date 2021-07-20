51a5e4b0f596d24515a067918dc386a0664a16a8
Yvan <Ivan.Gruber@seznam.cz>
2021-07-20 12:40:15 +0200

    HPOESSequentialDataset bug fixing

--- dataset/hpoes_dataset.py
+++ dataset/hpoes_dataset.py
@@ -293,7 +293,7 @@ class HPOESSequentialDataset(torch_data.Dataset):
     def get_indexes(self, idx):
         indexes = np.arange(self.sequence_length) + idx - ((self.sequence_length - 1) / 2)
         indexes[indexes < 0] = 0
-        indexes[indexes > len(self.labels)] = len(self.labels) - 1
+        indexes[indexes >= len(self.labels)] = len(self.labels) - 1
         return indexes.astype(int)
 
     def __getitem__(self, idx):
