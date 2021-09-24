bd59bc495fa3c026261f6d89c39c78f24e8740e6
Yvan <Ivan.Gruber@seznam.cz>
2021-09-24 13:18:36 +0200

    prediction_script.py update - using dataloader

--- potr_base/prediction_script.py
+++ potr_base/prediction_script.py
@@ -1,6 +1,6 @@
 
 """
-Standalone annotations script for hand pose estimation using POTR HPOES.
+Standalone annotations script for hand pose estimation using DETR HPOES.
 
 ! Warning !
 The script expects the input data in a .H5 dataset (under "images" key) in a form specified below. For other data
@@ -14,62 +14,59 @@ import h5py
 
 import numpy as np
 
-from potr_base.models.potr import POTR
-from potr_base.models.backbone import build_backbone
-from potr_base.models.transformer import build_transformer
+from torch.utils.data import DataLoader
+
+from deformable_potr.models.deformable_potr import DeformablePOTR
+from deformable_potr.models.backbone import build_backbone
+from deformable_potr.models.deformable_transformer import build_deformable_transformer
+
+from dataset.dataset_processing import aug_morph_close
+from dataset.hpoes_dataset import HPOESOberwegerDataset
 
 
 # Arguments
-parser = argparse.ArgumentParser("POTR HPOES Standalone Annotations Script", add_help=False)
+parser = argparse.ArgumentParser("DETR HPOES Standalone Annotations Script", add_help=False)
 parser.add_argument("--weights_file", type=str, default="out/checkpoint.pth",
                     help="Path to the pretrained model's chekpoint (.pth)")
 parser.add_argument("--input_file", type=str, default="in.h5",
                     help="Path to the .h5 file with input data (depth maps)")
 parser.add_argument("--output_file", type=str, default="out.h5", help="Path to the .h5 file to write into")
 parser.add_argument("--device", default="cpu", help="Device to be used")
+parser.add_argument("--tta", default=1, help="Whether to use Test Time Augmentation")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size")
 args = parser.parse_args()
 
 device = torch.device(args.device)
 
 # Load the input data and checkpoint
 print("Loading the input data and checkpoints.")
-input_datafile = h5py.File(args.input_file, "r")
+# input_datafile = h5py.File(args.input_file, "r")
 output_datafile = h5py.File(args.output_file, 'w')
 checkpoint = torch.load(args.weights_file, map_location=device)
 
-depth_maps_list = []
 output_list = []
 
-# Depth maps loading logic
-num_img = len(input_datafile["images"])
-# num_img = 1000
-for record_index in range(num_img):
-    print(record_index)
-    pdata = input_datafile["images"][str(record_index)][:].tostring()
-    _file = io.BytesIO(pdata)
-    data = np.load(_file)["arr_0"]
-    depth_maps_list.append(data)
-
-depth_maps = np.asarray(depth_maps_list)
-print("Structured input successfully.")
-
 # Construct the model from the loaded data
-model = POTR(
+model = DeformablePOTR(
     build_backbone(checkpoint["args"]),
-    build_transformer(checkpoint["args"]),
-    num_queries=checkpoint["args"].num_queries
+    build_deformable_transformer(checkpoint["args"]),
+    num_queries=checkpoint["args"].num_queries,
+    num_feature_levels=checkpoint["args"].num_feature_levels
 )
 model.load_state_dict(checkpoint["model"])
 model.to(device)
 
 print("Constructed model successfully.")
 
-# Iterate over the depth maps and structure the predictions
-for i, depth_map in enumerate(depth_maps):
-    dm_tensor = torch.from_numpy(depth_map)
-    dm_unsqueezed = dm_tensor.unsqueeze(0).expand(3, 224, 224).to(device, dtype=torch.float32)
+dataset_test = HPOESOberwegerDataset(args.input_file, encoded=True, mode='test')
+data_loader = DataLoader(dataset_test, batch_size=args.batch_size, num_workers=0, shuffle=True)
 
-    results = model([dm_unsqueezed]).detach().cpu().numpy()
+# Iterate over the depth maps and structure the predictions
+for i, (samples) in enumerate(data_loader):
+    print(i)
+    samples = samples.to(device, dtype=torch.float32)
+
+    results = model(samples).detach().cpu().numpy()
     output_list.append(results)
 
 print("Predictions were made.")
