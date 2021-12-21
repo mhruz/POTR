5b62bca70bdf71a6272cfe6ad4236ccfb11dfbe1
Matyáš Boháček <matyas.bohacek@matsworld.io>
2021-12-21 17:41:32 +0100

    Temp nested tensor fix

--- deformable_potr_plus/util/misc.py
+++ deformable_potr_plus/util/misc.py
@@ -331,6 +331,21 @@ def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
         for img, pad_img, m in zip(tensor_list, tensor, mask):
             pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
             m[: img.shape[1], :img.shape[2]] = False
+
+    if tensor_list[0].ndim == 2:
+        # TODO make it support different-sized images
+        max_size = _max_by_axis([list(img.shape) for img in tensor_list])
+        # min_size = tuple(min(s) for s in zip(*[img.shape for img in tensor_list]))
+        batch_shape = [len(tensor_list)] + max_size
+        b, h, w = batch_shape
+        dtype = tensor_list[0].dtype
+        device = tensor_list[0].device
+        tensor = torch.zeros(batch_shape, dtype=dtype, device=device)
+        mask = torch.ones((b, h, w), dtype=torch.bool, device=device)
+        for img, pad_img, m in zip(tensor_list, tensor, mask):
+            pad_img[: img.shape[0], : img.shape[1]].copy_(img)
+            m[: img.shape[0], :img.shape[1]] = False
+
     else:
         raise ValueError('not supported')
     return NestedTensor(tensor, mask)
