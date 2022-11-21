new accuracy computation in mm using cubes, various bug-fixes

dataset/dataset_processing.py
@@ -42,6 +42,7 @@ def load_hpoes_data(filename: str, mode: str):
 
     output_depth_maps = []
     output_labels = []
+    output_cubes = []
 
     for record_index in range(len(data_file["images"])):
         pdata = data_file["images"][str(record_index)][:].tostring()
@@ -49,11 +50,13 @@ def load_hpoes_data(filename: str, mode: str):
         data = np.load(_file)["arr_0"]
         output_depth_maps.append(data)
 
+        output_cubes.append(data_file["cube"][record_index])
+
         if mode != 'test':
             labels = data_file["labels"][record_index]
             output_labels.append(labels)
 
-    return {"data": output_depth_maps, "labels": output_labels}
+    return {"data": output_depth_maps, "labels": output_labels, "cubes": output_cubes}
 
 
 def load_encoded_hpoes_data(filename: str, mode: str):
@@ -73,16 +76,19 @@ def load_encoded_hpoes_data(filename: str, mode: str):
 
     output_depth_maps = []
     output_labels = []
+    output_cubes = []
 
     for record_index in range(len(data_file["images"])):
         pdata = data_file["images"][str(record_index)][:].tostring()
         output_depth_maps.append(pdata)
 
+        output_cubes.append(data_file["cube"][record_index])
+
         if mode != 'test':
             labels = data_file["labels"][record_index]
             output_labels.append(labels)
 
-    return {"data": output_depth_maps, "labels": output_labels}
+    return {"data": output_depth_maps, "labels": output_labels, "cubes": output_cubes}
 
 
 def aug_dilate(image, **kwargs):

dataset/hpoes_dataset.py
@@ -196,6 +196,7 @@ class HPOESOberwegerDataset(torch_data.Dataset):
         self.labels = labels
         self.transform = transform
         self.mode = mode
+        self.cubes = loaded_data["cubes"]
 
     @staticmethod
     def preprocessing():
@@ -230,10 +231,13 @@ class HPOESOberwegerDataset(torch_data.Dataset):
             depth_map = transformed["image"]
             depth_map = torch.from_numpy(depth_map)
             depth_map = depth_map.unsqueeze(0).expand(3, self.data_resolution, self.data_resolution)
+
+            cube = torch.Tensor(self.cubes[idx])
+
             if label is not None:
                 label = torch.from_numpy(np.asarray(label))
-                return depth_map, label
-            return depth_map
+                return depth_map, label, cube
+            return depth_map, cube
 
         # Perform any additionally desired transformations
         if self.transform:
@@ -277,7 +281,9 @@ class HPOESOberwegerDataset(torch_data.Dataset):
 
         label = torch.from_numpy(np.asarray(label))
 
-        return depth_map, label
+        cube = torch.Tensor(self.cubes[idx])
+
+        return depth_map, label, cube
 
     def __len__(self):
         return len(self.data)

deformable_potr_plus/engine.py
@@ -25,14 +25,13 @@ from deformable_potr_plus.datasets.data_prefetcher import data_prefetcher
 
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, log_wandb=False):
+                    device: torch.device, epoch: int, log_wandb=False, print_freq=200):
     model.train()
     criterion.train()
     header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 200
     losses_all = []
 
-    for item_index, (samples, targets) in enumerate(data_loader):
+    for item_index, (samples, targets, cubes) in enumerate(data_loader):
         samples = [item.to(device, dtype=torch.float32) for item in samples]
         targets = [item.to(device) for item in targets]
 
@@ -76,7 +75,7 @@ def evaluate(model, criterion, data_loader, device, print_freq=10, log_wandb=Fal
     l2_pred_error_distances = []
     l2_pred_error_distances_result_averaging = []
 
-    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+    for samples, targets, cubes in metric_logger.log_every(data_loader, print_freq, header):
         samples = [item.to(device, dtype=torch.float32) for item in samples]
         targets = [item.to(device) for item in targets]
 
@@ -84,7 +83,7 @@ def evaluate(model, criterion, data_loader, device, print_freq=10, log_wandb=Fal
         loss_dict = criterion(outputs, targets)
 
         # Calculate L2 distance once outputs are matches using Hungarian algorithm
-        l2_pred_error_distances.append(criterion.get_avg_L2_prediction_error_hungarian_matcher_decoding(outputs, targets))
+        l2_pred_error_distances.append(criterion.get_avg_L2_prediction_error_hungarian_matcher_decoding(outputs, targets, cubes))
 
         # Calculate L2 distance for output averaging per predicted class
         l2_pred_error_distances_result_averaging.append(criterion.calculate_avg_L2_distance(model.convert_outputs_average_decoding(outputs), torch.stack(targets)))

deformable_potr_plus/models/deformable_potr_plus.py
@@ -313,7 +313,7 @@ class SetCriterion(nn.Module):
 
         targets = [{
             "coords": target,
-            "labels": torch.tensor(list(range(14)))
+            "labels": torch.tensor(list(range(target.shape[0])))
         } for target in targets]
 
         indices = self.matcher(outputs, targets)
@@ -324,26 +324,28 @@ class SetCriterion(nn.Module):
 
         return losses
 
-    def get_avg_L2_prediction_error_hungarian_matcher_decoding(self, outputs, targets):
+    def get_avg_L2_prediction_error_hungarian_matcher_decoding(self, outputs, targets, cubes=None):
         """
         Decodes the outputs using Hungarian matcher and calculates the average L2 distance (prediction error) per batch
         per single joint.
 
         :param outputs: Outputs from the model
         :param targets: Target joint coordinates
+        :param cubes: Cube sizes of individual data-points, if not provided, the default value is used
         :return: Average prediction error (in millimeters)
         """
 
         targets = [{
             "coords": target,
-            "labels": torch.tensor(list(range(14)))
+            "labels": torch.tensor(list(range(target.shape[0])))
         } for target in targets]
 
         indices = self.matcher(outputs, targets)
 
         idx = self._get_src_permutation_idx(indices)
-        src_coords = outputs["pred_coords"][idx]
-        target_coords = torch.cat([t["coords"][i] for t, (_, i) in zip(targets, indices)], dim=0)
+        # src_coords = outputs["pred_coords"][idx]
+        src_coords = (outputs["pred_coords"] * cubes.unsqueeze(1).to(outputs["pred_coords"].device) / 2.0)[idx]
+        target_coords = torch.cat([t["coords"][i] * cubes[c].to(t["coords"].device) for c, (t, (_, i)) in enumerate(zip(targets, indices))], dim=0)
 
         return self.calculate_avg_L2_distance(src_coords, target_coords)
 
@@ -357,20 +359,17 @@ class SetCriterion(nn.Module):
         """
 
         try:
-            # Obtain the average L2 distance per a single joint estimation (therefore the overall result is divided by
-            # the number of targets and classes) and convert this relative distance form (as the dataset target values
-            # are on the [-1; 1] range to absolute mms
+            # Obtain the average L2 distance per a single joint estimation
             loss_coords = F.mse_loss(output_coords, target_coords, reduction="none")
 
             mean_loss_coords = float(torch.mean(torch.sqrt(torch.sum(loss_coords, dim=1))))
-            res = mean_loss_coords * (self.cube_size * 0.5)
 
         except Exception as e:
             print(e)
             logging.warning("A problem occurred during L2 error calculation!")
             return 0
 
-        return res
+        return mean_loss_coords
 
 
 class PostProcess(nn.Module):