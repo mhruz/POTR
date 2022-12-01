cf549c46fd6f19b855b84cb882ad237e537a62fd
mighty <m@m.hr>
2022-12-01 16:32:28 +0100

    new prediction_script.py (ZKR)

---
+++ deformable_potr_plus/models/matcher_prediction.py
@@ -0,0 +1,81 @@
+# ------------------------------------------------------------------------
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# ------------------------------------------------------------------------
+
+"""
+Modules to compute the matching cost and solve the corresponding LSAP.
+"""
+import torch
+from scipy.optimize import linear_sum_assignment
+from torch import nn
+
+
+class HungarianMatcher(nn.Module):
+    """This class computes an assignment between the targets and the predictions of the network
+
+    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
+    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
+    while the others are un-matched (and thus treated as non-objects).
+    """
+
+    def __init__(self, cost_class: float = 1, cost_coord: float = 1):
+        """Creates the matcher
+
+        Params:
+            cost_class: This is the relative weight of the classification error in the matching cost
+            cost_bbox: This is the relative weight of the L1 error of the bounding box coordinates in the matching cost
+            cost_giou: This is the relative weight of the giou loss of the bounding box in the matching cost
+        """
+
+        super().__init__()
+        self.cost_class = cost_class
+        self.cost_coord = cost_coord
+
+        assert cost_class != 0 or cost_coord != 0
+
+    def forward(self, outputs):
+        """ Performs the matching
+
+        Params:
+            outputs: This is a dict that contains at least these entries:
+                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
+                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
+
+            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
+                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
+                           objects in the target) containing the class labels
+                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
+
+        Returns:
+            A list of size batch_size, containing tuples of (index_i, index_j) where:
+                - index_i is the indices of the selected predictions (in order)
+                - index_j is the indices of the corresponding selected targets (in order)
+            For each batch element, it holds:
+                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
+        """
+
+        with torch.no_grad():
+
+            result = nn.functional.softmax(outputs["pred_logits"], dim=2)
+
+            out = []
+            for pred_logits, pred_coords in zip(result, outputs["pred_coords"]):
+                C = pred_logits.cpu().numpy()
+                i, j = linear_sum_assignment(C.T, maximize=True)
+
+                p = pred_coords.detach().cpu().numpy()
+                # don't care about the last class (the background)
+                p = p[j[:-1]]
+
+                out.append(p)
+
+            return out
+
+
+def build_matcher():
+    return HungarianMatcher()
--- deformable_potr_plus/prediction_script.py
+++ deformable_potr_plus/prediction_script.py
@@ -9,16 +9,19 @@ structures, please implement your own logic. Either way, the `depth_maps` should
 
 import argparse
 import torch
-import io
 import h5py
 
 import numpy as np
 
+from torch.utils.data import DataLoader
+
 from deformable_potr_plus.models.deformable_potr_plus import DeformablePOTR
 from deformable_potr_plus.models.backbone import build_backbone
 from deformable_potr_plus.models.deformable_transformer import build_deformable_transformer
+from deformable_potr_plus.models.matcher_prediction import build_matcher
 
-from dataset.dataset_processing import aug_morph_close
+from dataset.hpoes_dataset import HPOESOberwegerDataset
+
 
 # Arguments
 parser = argparse.ArgumentParser("DETR HPOES Standalone Annotations Script", add_help=False)
@@ -28,56 +31,57 @@ parser.add_argument("--input_file", type=str, default="in.h5",
                     help="Path to the .h5 file with input data (depth maps)")
 parser.add_argument("--output_file", type=str, default="out.h5", help="Path to the .h5 file to write into")
 parser.add_argument("--device", default="cpu", help="Device to be used")
-parser.add_argument("--tta", default=1, help="Whether to use Test Time Augmentation")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size")
+parser.add_argument('--set_cost_class', default=2, type=float, help="Class coefficient in the matching cost")
+parser.add_argument('--set_cost_coord', default=5, type=float, help="Coordinate distance coefficient in the matching cost")
+parser.add_argument('--data_resolution', default=(224, 224), type=int, nargs=2, help="Resolution of the input data.")
 args = parser.parse_args()
 
 device = torch.device(args.device)
 
 # Load the input data and checkpoint
 print("Loading the input data and checkpoints.")
-input_datafile = h5py.File(args.input_file, "r")
 output_datafile = h5py.File(args.output_file, 'w')
 checkpoint = torch.load(args.weights_file, map_location=device)
 
-depth_maps_list = []
 output_list = []
 
-# Depth maps loading logic
-num_img = len(input_datafile["images"])
-# num_img = 1000
-for record_index in range(num_img):
-    print(record_index)
-    pdata = input_datafile["images"][str(record_index)][:].tostring()
-    _file = io.BytesIO(pdata)
-    data = np.load(_file)["arr_0"]
-    depth_maps_list.append(data)
-
-depth_maps = np.asarray(depth_maps_list)
-print("Structured input successfully.")
-
 # Construct the model from the loaded data
+backbone = build_backbone(checkpoint["args"])
+matcher_prediction = build_matcher()
+transformer = build_deformable_transformer(checkpoint["args"])
 model = DeformablePOTR(
-    build_backbone(checkpoint["args"]),
-    build_deformable_transformer(checkpoint["args"]),
-    num_queries=checkpoint["args"].num_queries,
-    num_feature_levels=checkpoint["args"].num_feature_levels
-)
+        backbone,
+        transformer,
+        num_classes=checkpoint["args"].num_classes + 1,  # Add one additional class for representation of the background
+        num_queries=checkpoint["args"].num_queries,
+        num_feature_levels=checkpoint["args"].num_feature_levels,
+    )
+
 model.load_state_dict(checkpoint["model"])
+model.eval()
 model.to(device)
 
 print("Constructed model successfully.")
 
+dataset_test = HPOESOberwegerDataset(args.input_file, data_resolution=args.data_resolution, encoded=True, mode='eval')
+data_loader = DataLoader(dataset_test, batch_size=args.batch_size, num_workers=0, shuffle=False)
+
 # Iterate over the depth maps and structure the predictions
-for i, depth_map in enumerate(depth_maps):
-    # test time augmentation - morphological close
-    if args.tta == 1:
-        depth_map = aug_morph_close(depth_map)
+for i, (samples, targets, cubes) in enumerate(data_loader):
+    print(i)
+    samples = [item.to(device, dtype=torch.float32) for item in samples]
+    targets = [item.to(device) for item in targets]
+    '''targets = [{
+            "coords": target.to(device),
+            "labels": torch.tensor(list(range(target.shape[0]))).to(device)
+        } for target in targets]'''
 
-    dm_tensor = torch.from_numpy(depth_map)
-    dm_unsqueezed = dm_tensor.unsqueeze(0).expand(3, 224, 224).to(device, dtype=torch.float32)
+    results = model(samples)
 
-    results = model([dm_unsqueezed]).detach().cpu().numpy()
-    output_list.append(results)
+    out = matcher_prediction(results)
+
+    output_list.extend(out)
 
 print("Predictions were made.")
 
